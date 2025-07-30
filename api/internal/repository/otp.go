010b4f50666b6f97bd05fe4218fcb725c72fd74b
FishHawk <whang23@outlook.com>
2025-07-30 22:03:46 +0800

    chore(api): 重置密码的验证码改成26位字母数字

--- api/internal/repository/otp.go
+++ api/internal/repository/otp.go
@@ -2,8 +2,9 @@ package repository
 
 import (
 	"context"
+	"crypto/rand"
 	"fmt"
-	"math/rand"
+	"math/big"
 	"time"
 
 	"github.com/redis/go-redis/v9"
@@ -29,17 +30,33 @@ func NewOtpRepository(rdb *redis.Client) OtpRepository {
 	}
 }
 
-func createOtp() string {
-	randomNum := rand.Intn(999999)
-	return fmt.Sprintf("%06d", randomNum)
+func createOtp(otpType string) (string, error) {
+	switch otpType {
+	case OtpVerify:
+		n, err := rand.Int(rand.Reader, big.NewInt(1_000_000))
+		if err != nil {
+			return "", err
+		}
+		return fmt.Sprintf("%06d", n.Int64()), nil
+	case OtpResetPassword:
+		return rand.Text(), nil
+	default:
+		return "", fmt.Errorf("unknown otp type: %s", otpType)
+	}
 }
 
 var ctx = context.Background()
 
 func (r *otpRepository) SetOtp(otpType string, email string) (string, error) {
-	otp := createOtp()
-	err := r.rdb.Set(ctx, otpType+":"+email, otp, time.Minute*15).Err()
-	return otp, err
+	otp, err := createOtp(otpType)
+	if err != nil {
+		return "", err
+	}
+	err = r.rdb.Set(ctx, otpType+":"+email, otp, time.Minute*15).Err()
+	if err != nil {
+		return "", err
+	}
+	return otp, nil
 }
 
 func (r *otpRepository) CheckOtp(otpType string, email, otp string) bool {
