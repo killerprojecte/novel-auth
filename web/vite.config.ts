264a8b010351a1755a2bb5fe9effaf1c179bb4ee
FishHawk <whang23@outlook.com>
2025-08-02 23:14:09 +0800

    chore(web): 调整打包方式

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -2,7 +2,6 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 import tailwindcss from "@tailwindcss/vite";
 import Components from "unplugin-svelte-components/vite";
 import { defineConfig } from "vite";
-import { viteSingleFile } from "vite-plugin-singlefile";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -16,7 +15,6 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
     svelte(),
-    viteSingleFile(),
     Components({
       dts: "src/components.d.ts",
       dirs: ["src/components", "src/ui"],
