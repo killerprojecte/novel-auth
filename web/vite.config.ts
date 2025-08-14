7a0ed5077a6a80d9f14611a5041075cff04a2231
FishHawk <whang23@outlook.com>
2025-08-15 02:53:04 +0800

    feat(web): 添加css层叠表的polyfill，修复旧浏览器的格式问题

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,35 +1,47 @@
+import postcssCascadeLayers from '@csstools/postcss-cascade-layers';
 import tailwindcss from '@tailwindcss/vite';
 import vue from '@vitejs/plugin-vue';
 import AutoImport from 'unplugin-auto-import/vite';
 import Components from 'unplugin-vue-components/vite';
-import { defineConfig } from 'vite';
+import { defineConfig, type UserConfig } from 'vite';
 import { createHtmlPlugin } from 'vite-plugin-html';
 
 // https://vite.dev/config/
-export default defineConfig({
-  build: {
-    target: ['es2015'],
-  },
-  server: {
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': '*',
-      'Access-Control-Allow-Headers': 'Content-Type',
+export default defineConfig(({ mode }) => {
+  const config: UserConfig = {
+    build: {
+      target: ['es2015'],
     },
-  },
-  plugins: [
-    tailwindcss(),
-    vue(),
-    createHtmlPlugin({
-      minify: { minifyJS: true },
-    }),
-    AutoImport({
-      dts: './src/auto-imports.d.ts',
-      imports: ['vue', 'vue-router', 'pinia'],
-    }),
-    Components({
-      dts: 'src/components.d.ts',
-      dirs: ['src/components', 'src/ui'],
-    }),
-  ],
+    server: {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': '*',
+        'Access-Control-Allow-Headers': 'Content-Type',
+      },
+    },
+    css: {},
+    plugins: [
+      tailwindcss(),
+      vue(),
+      createHtmlPlugin({
+        minify: { minifyJS: true },
+      }),
+      AutoImport({
+        dts: './src/auto-imports.d.ts',
+        imports: ['vue', 'vue-router', 'pinia'],
+      }),
+      Components({
+        dts: 'src/components.d.ts',
+        dirs: ['src/components', 'src/ui'],
+      }),
+    ],
+  };
+
+  // postcss-cascade-layers 在开发模式下会导致样式加载异常，因此仅在生产模式下启用
+  if (mode === 'production') {
+    config.css!.postcss = {
+      plugins: [postcssCascadeLayers()],
+    };
+  }
+  return config;
 });
